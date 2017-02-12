fd09f72e0437dcc10faee6db3de24380e0c4a3b3
Laiter <laitergg@ya.ru>
2017-02-12 16:21:48 +0300

    Fix class logic issue, add comments and functional
    add a lot of comments to describe class methods and functions
    fix undo() issue with ^operation caused by wrong flag_ logic
    fix =operation logic in functor and call_calc() to work not only as show
    result but and assign result
    add set and get_flag() functions
    add default constructor
    other minor fixes with code formatting

--- Calc/Calc/simple_calulator.cpp
+++ Calc/Calc/simple_calulator.cpp
@@ -2,9 +2,18 @@
 #include <string>
 
 namespace simplecalc {
-char SimpleCalculator::calc_last_sign_;
-double SimpleCalculator::calc_last_arg_;
-double SimpleCalculator::calc_result_;
+char SimpleCalculator::calc_last_sign_ = 0;
+double SimpleCalculator::calc_last_arg_ = 0;
+double SimpleCalculator::calc_result_ = 0;
+bool SimpleCalculator::flag_ = 0;
+
+SimpleCalculator::SimpleCalculator()
+{
+	calc_last_sign_ = '=';
+	calc_last_arg_ = 0;
+	calc_result_ = 0;
+	flag_ = 0;
+}
 
 SimpleCalculator::SimpleCalculator(char sign)
 {
@@ -15,10 +24,11 @@ SimpleCalculator::~SimpleCalculator()
 {
 }
 
-double SimpleCalculator::operator()(double operand_lh, double operand_rh)
-{
-	calc_result_ = 0;
+double SimpleCalculator::operator()(double operand_lh, double operand_rh) //simple functor that clears old results 
+{																		  //and call switch with possible operations
+	calc_result_ = 0;													  //that work with 2 functor parameters
 	calc_last_arg_ = 0;
+	flag_ = 0;
 	switch (calc_last_sign_) {
 	case '+':
 		calc_last_arg_ = operand_rh;
@@ -41,19 +51,16 @@ double SimpleCalculator::operator()(double operand_lh, double operand_rh)
 		}
 		return calc_result_ = mult;
 	}
-	case '=':
-	{
-		return calc_result_;
-	}
 	default:
 		std::cout << "Operation's not allowed\n";
 		return 0;
 	}
 } //operator()(double operand_lh, double operand_rh)
 
-double SimpleCalculator::operator()(double operand_rh)
-{
-	switch (calc_last_sign_) {
+double SimpleCalculator::operator()(double operand_rh) 	//simple functor that doesn't clears old results 
+{														//and call switch with possible operations
+	flag_ = 0;											//that work with functor parameter and class result
+	switch (calc_last_sign_) {							
 	case '+':
 		calc_last_arg_ = operand_rh;
 		return calc_result_ += operand_rh;
@@ -77,7 +84,8 @@ double SimpleCalculator::operator()(double operand_rh)
 	}
 	case '=':
 	{
-		return calc_result_;
+		calc_last_arg_ = calc_result_;
+		return calc_result_ = operand_rh;
 	}
 	default:
 		std::cout << "Operation's not allowed\n";
@@ -85,8 +93,8 @@ double SimpleCalculator::operator()(double operand_rh)
 	}
 } //operator()(double operand_rh)
 
-double SimpleCalculator::undo()
-{
+double SimpleCalculator::undo() //undo last operation by opposite operation with
+{								//saved result, argument and sign
 	switch (calc_last_sign_) {
 	case '+':
 		calc_result_ -= calc_last_arg_;
@@ -108,8 +116,7 @@ double SimpleCalculator::undo()
 	{
 		double mult = calc_result_;
 		double power = calc_last_arg_;
-		static bool flag = 0;
-		if (flag == 0)
+		if (flag_ == 0)
 		{
 			calc_result_ = pow(mult, (1 / power));
 		}
@@ -117,7 +124,12 @@ double SimpleCalculator::undo()
 		{
 			calc_result_ = pow(mult, power);
 		}
-		flag ^= 1;
+		flag_ ^= 1;
+		return calc_result_;
+	}
+	case '=':
+	{
+		std::swap(calc_last_arg_, calc_result_);
 		return calc_result_;
 	}
 	default:
@@ -141,6 +153,11 @@ char SimpleCalculator::get_last_sign()
 	return calc_last_sign_;
 }
 
+bool SimpleCalculator::get_flag()
+{
+	return flag_;
+}
+
 void SimpleCalculator::set_res(double result)
 {
 	calc_result_ = result;
@@ -156,13 +173,20 @@ void SimpleCalculator::set_last_sign(char last_sign)
 	calc_last_sign_ = last_sign;
 }
 
-void SimpleCalculator::call_info()
+void SimpleCalculator::set_flag(bool flag)
 {
-	std::cout << "info:\n"
-		<< "Enter - show info\n"
-		<< "Ctrl+Q or Ctrl+Z - exit\n"
-		<< "Possible expressions: \n<operand><operation><operand> - ex. 10+2 (clear last result)\n<operation><operand> - ex. +10 (save last result)"
-		<< "\nPossible operations:\n +, -, *, /, ^, =, undo\n";
+	flag_ = flag;
+}
+
+void call_info()
+{
+	std::cout << "info:" << std::endl
+		<< "Enter - show info" << std::endl
+		<< "Ctrl+Q or Ctrl+Z - exit" << std::endl
+		<< "Possible expressions:" << std::endl
+		<< "<operand><operation><operand> - ex. 10+2 (clear last result)" << std::endl
+		<< "<operation><operand> - ex. +10 (save last result)" << std::endl
+		<< "Possible operations:\n +, -, *, /, ^, =, undo" << std::endl;
 }
 
 void call_calc()
@@ -171,16 +195,16 @@ void call_calc()
 	double op1, op2;
 	std::string expression;
 	std::size_t found;
-	do {
+	do { // inf cycle, exit with Ctrl+Z or Ctrl+Q
 		found = 0;
 		expression.clear();
-		std::cout << "SimpleCalculator: " << std::flush;
-		std::getline(std::cin, expression);
-		if (std::cin.eof() || expression[0] == '\x11')
+		std::cout << "SimpleCalculator: ";
+		std::getline(std::cin, expression); //write expression in console
+		if (std::cin.eof() || expression[0] == '\x11') //check exit Ctrl+Z or Ctrl+Q
 		{
 			break;
 		}
-		if (isdigit(expression[0]))
+		if (isdigit(expression[0])) //if first is digit, call functor with 2 parameters
 		{
 			if ((found = expression.find_first_of("+-*/^")) != std::string::npos)
 			{
@@ -197,37 +221,46 @@ void call_calc()
 				std::cout << op1 << ' ' << operation << ' ' << op2 << " = "
 					<< SimpleCalculator(operation)(op1, op2) << std::endl;
 			}
-			else
+			else // continue cycle if expression is wrong or can't be read
 			{
 				continue;
 			}
 		}
-		else if ((found = expression.find_first_of("+-*/^=")) != std::string::npos)
-		{
+		else if ((found = expression.find_first_of("+-*/^=")) != std::string::npos) //else if found supported sign, 
+		{																			//call functor with 1 parameters
 			operation = expression[found];
-			if (isdigit(expression[found + 1]))
+			
+			if (expression[found] == '=')
+			{
+				std::cout << "last result(" << SimpleCalculator::get_res() << ")";
+				if (isdigit(expression[found + 1]))
+				{
+					op2 = std::stod(expression.substr(found + 1));
+					std::cout << " " << operation << " " << SimpleCalculator(operation)(op2) << std::endl;
+					continue;
+				}
+				std::cout << std::endl;
+				continue;
+				
+			}
+			else if (isdigit(expression[found + 1]))
 			{
 				op2 = std::stod(expression.substr(found + 1));
 			}
-			else if (expression[found] == '=')
-			{
-				std::cout << "last result(" << SimpleCalculator::get_res() << ") \n";
-				continue;
-			}
-			else
+			else // continue cycle if expression is wrong or can't be read
 			{
 				continue;
 			}
 			std::cout << "last result(" << SimpleCalculator::get_res() << ") " << operation << " " << op2 << " = ";
 			std::cout << SimpleCalculator(operation)(op2) << std::endl;
 		}
-		else if (found = expression.find("undo") != std::string::npos)
+		else if (found = expression.find("undo") != std::string::npos) //else if found word "undo", call static function undo()
 		{
 			std::cout << "previous result(" << SimpleCalculator::undo() << ") \n";
 		}
-		else
+		else //else call function call_info()
 		{
-			SimpleCalculator::call_info();
+			call_info();
 			continue;
 		}
 	} while (true);
--- Calc/Calc/simple_сalculator.h
+++ Calc/Calc/simple_сalculator.h
@@ -1,13 +1,16 @@
 #pragma once
 #include <iostream>
 #include <cmath>
+#include <utility>
 namespace simplecalc {
 class SimpleCalculator
 {
 	static char calc_last_sign_;
 	static double calc_last_arg_;
 	static double calc_result_;
+	static bool flag_;
 public:
+	SimpleCalculator();
 	SimpleCalculator(char sign);
 	~SimpleCalculator();
 	//methods
@@ -15,15 +18,16 @@ public:
 	static double get_res();
 	static double get_last_arg();
 	static char get_last_sign();
+	static bool get_flag();
 	static void set_res(double result);
 	static void set_last_arg(double last_arg);
 	static void set_last_sign(char last_sign);
-	static void call_info();
+	static void set_flag(bool flag);
 	//functors
 	double operator()(double operand_lh, double operand_rh);
 	double operator()(double operand_rh);
 };
-
+void call_info();
 void call_calc();
 } //namespace simplecalc
 

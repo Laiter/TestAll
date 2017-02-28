9d20442bcb5e0f1697556b830d7a72d4d6e743f8
Laiter <laitergg@ya.ru>
2017-02-28 23:54:43 +0300

    Update class Encryption for release
    add licence
    add README.txt
    remove deprecated files from solution
    add test.txt

--- EncryptionLaiter/EncryptionLaiter/main.cpp
+++ EncryptionLaiter/EncryptionLaiter/main.cpp
@@ -1,3 +1,20 @@
+///////////////////////////////////////////////////////////////////////////////////
+//Copyright(c) 2017 Laiter. @Vladislav Sarychev
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http ://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+///////////////////////////////////////////////////////////////////////////////////
+//author: @Vladislav Sarychev
+//version: 1.0 28.02.17
 //******************************************************************************************
 // v1.0: Encrypter/Decrypter
 // 1. write a class Encryption that should read\write and encrypt\decrypt any file. 
@@ -11,6 +28,7 @@
 // for the future releases:
 // TODO: develop better key algorithms
 // TODO: develop GUI
+// TODO: port code to Android
 #include "encryption.h"
 
 void main()
